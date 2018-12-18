c71fdd7049da11544650019aa4f871c8683bf269
pivettes <pivettes@gmail.com>
2018-12-18 16:54:08 +0100

    config changes

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+import sys
+sys.path.append("C:\\Users\\spivette\\AppData\\Local\\Programs\\Python\\Python37\\python.exe")
+sys.path.append("C:\\Users\\pivet\\AppData\\Local\\Programs\\Python\\Python37\\python.exe")
 
 from resources.lib import kodilogging
 from resources.lib import plugin
 
 import logging
 import xbmcaddon
+
+
 
 # Keep this file to a minimum, as Kodi
 # doesn't keep a compiled copy of this
